39c65458d50b75298b26dcba8210f94ead76f5a1
rektide <rektide@voodoowarez.com>
2019-12-04 00:39:28 -0500

    first intcode computer

---
+++ 2/1.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const codes= process.argv.slice( 2).map( n=> parseInt( n))
+let pos= 0
+
+console.log(codes)
+
+function step(){
+	let
+	  op= codes[ pos],
+	  lPos= codes[ pos+ 1],
+	  l= codes[ lPos],
+	  rPos= codes[ pos+ 2],
+	  r= codes[ rPos],
+	  dest= codes[ pos+ 3],
+	  res
+	pos+= 4
+	if( op=== 1){
+		res= l+ r
+	}else if( op=== 2){
+		res= l* r
+	}else if( op=== 99){
+		console.log( codes.join("\n"))
+		process.exit( 0)
+	}else{
+		console.error( "unknown intcode, system halt")
+		process.exit( 1)
+	}
+	codes[ dest]= res
+	//console.error(`${op} ${l} ${r} ${res} ${dest}`)
+}
+
+while( true){
+	step()
+}
---
+++ 2/1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat ${1:-input} | tr ',' '\n' | sed '2s/.*/12/' | sed '3s/.*/2/' | xargs node 1.js
