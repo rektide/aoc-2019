3b4dc52954183c0b5be1763d0bd5e868762dc611
rektide <rektide@voodoowarez.com>
2019-12-04 04:20:40 -0500

    day 4: brute force password tests
    was going to try & get creative & subdivide ranges somehow but yeahh no

--- 4/pw.js
+++ 4/pw.js
@@ -4,7 +4,33 @@ const
   max= parseInt( process.argv[ 3]),
   max_= max
 
-function possibilities( min= min_, max= max_, hasDouble= true){
-	
+console.log({ min, max})
+
+function check( n){
+	const str= new String( n)
+	let
+	  prev,
+	  hasDouble= false
+	for( let i= str.length; i>= 0; --i){
+		let cur= str[ i]
+		if( prev){
+			if( prev< cur){
+				return false
+			}
+			if( prev=== cur){
+				hasDouble= true
+			}
+		}
+		prev= cur
+	} 
+	return hasDouble
 }
-console.log( possibilities())
+
+let possible= 0
+for( let i= min; i< max; ++i){
+	if( check( i)){
+		++possible
+		console.error( i)
+	}
+}
+console.log( possible)
