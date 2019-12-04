c8a9c00bc86437f4965422ce2eb1966bf6fffed5
rektide <rektide@voodoowarez.com>
2019-12-04 03:34:27 -0500

    day 3: map second wire while looking for best intersection

--- 3/manhattan.js
+++ 3/manhattan.js
@@ -19,7 +19,7 @@ export function parseWire( wire){
 		})
 }
 
-export function mapWire( wire, area= [], cb, n= 1){
+export function mapWire( wire, area= [], n= 1, cb){
 	let
 	  x= 0,
 	  y= 0
@@ -33,7 +33,7 @@ export function mapWire( wire, area= [], cb, n= 1){
 				// already seen this wire here
 				return
 			}
-			cb&& cb( x, y, had, area)
+			cb&& cb({ x, y, had, area})
 			had.push( n)
 		}else{
 			area[ x][ y]= [ n]
@@ -71,10 +71,29 @@ export function mapWire( wire, area= [], cb, n= 1){
 }
 
 export function main(){
+	// read first wire
 	const
 	  w1= parseWire( process.argv[ 2]),
 	  w2= parseWire( process.argv[ 3]),
 	  area= mapWire( w1)
+
+	// read second wire & look for match
+	let
+	  x,
+	  y,
+	  d= Number.POSITIVE_INFINITY
+	mapWire( w2, area, 2, function({ x: xB, y: yB}){
+		const dB= Math.abs( xB)+ Math.abs( yB)
+		if( dB=== 0){
+		}else if( dB< d){
+			d= dB
+			x= xB
+			y= yB
+			console.error({ x: xB, y: yB, dB, best: true}) // errlog intersection
+		}else{
+			console.error({ x: xB, y: yB, dB}) // errlog intersection
+		}
+	})
+	console.log({ d, x, y})
 }
 main()
-
